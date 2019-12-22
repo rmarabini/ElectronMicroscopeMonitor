4e5de36bf705dc466b7f32b57645f1ae83eb50af
rmarabini <you@example.com>
2019-12-22 20:12:53 +0100

    create env_log_base with the functions that log in influxdb

--- rpiWebServer/mag_log_influxdb.py
+++ rpiWebServer/mag_log_influxdb.py
@@ -10,8 +10,18 @@ from constants import (dictSensors, MIC, RASPB, MEASUREMENTTMP)
 from log_base import log_values
 DEBUG=True
 
+try:
+    from bayeosraspberrypi.mcp3424 import MCP3424
+    from bayeosraspberrypi.i2c import I2C
+except:
+    print("bayeosraspberrypi package is needed")
+import time
+import os
 
-# read temperature from probes and create a dictionary
+
+
+
+# read temperature from mag probes and create a dictionary
 fields={}
 for key, sensor in dictSensors.items():
     if DEBUG:  # testing, do not access to probes
