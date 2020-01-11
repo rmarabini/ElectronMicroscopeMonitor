ada3031fde5d2b15d726746e326233e054939cbe
Tu Nombre <you@example.com>
2020-01-11 19:35:44 +0100

    added new tmp probe

--- Scripts/tempEachSec.py
+++ Scripts/tempEachSec.py
@@ -4,7 +4,8 @@ the temperature once every second:"""
 import time
 
 listSensors = ["/sys/bus/w1/devices/28-01144fd138aa/w1_slave",  # one
-               "/sys/bus/w1/devices/28-0114509470aa/w1_slave"]  # two
+               "/sys/bus/w1/devices/28-0114509470aa/w1_slave",  # two
+               "/sys/bus/w1/devices/28-01144fcdf4aa/w1_slave"]  # three
 
 while 1:
     for sensor in listSensors:
